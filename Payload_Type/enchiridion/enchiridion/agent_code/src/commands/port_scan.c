5d363486cad969a42c9d8f4e3bdbf01f821b427d
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2026-06-22 15:12:43 -0400

    feat: broken example code for initial dev

---
+++ Payload_Type/enchiridion/enchiridion/agent_code/src/commands/port_scan.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    int sockfd;
+    struct sockaddr_in serv_addr;
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+
+    for (int port = 1; port <= 1024; port++) {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            perror("Socket creation failed");
+            continue;
+        }
+        serv_addr.sin_port = htons(port);
+
+        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == 0) {
+            printf("Port %d is open\n", port);
+        }
+        close(sockfd);
+    }
+    return 0;
+}
