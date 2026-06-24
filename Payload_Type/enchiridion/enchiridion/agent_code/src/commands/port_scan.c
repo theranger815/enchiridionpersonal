88116b9c34a8aab0cb96f012dd51c1a2b72ed9af
PrettyBoyCosmo <cosmodiumcs@gmail.com>
2026-06-24 15:39:52 -0400

    fix: port printing again

--- Payload_Type/enchiridion/enchiridion/agent_code/src/commands/port_scan.c
+++ Payload_Type/enchiridion/enchiridion/agent_code/src/commands/port_scan.c
@@ -32,36 +32,68 @@ int scan(int port, int sockfd, struct sockaddr_in serv_addr) {
     return port;
 }
 
+static void appendPort(char **buf, size_t *cap, size_t *len, int port) {
+    char line[16];
+    int line_len = snprintf(line, sizeof(line), "%d\n", port);
+
+    while (*len + (size_t)line_len + 1 > *cap) {
+        *cap *= 2;
+        *buf = realloc(*buf, *cap);
+    }
+
+    memcpy(*buf + *len, line, (size_t)line_len);
+    *len += (size_t)line_len;
+}
+
 void portScan(char *host, char *port, char *mode, TaskResponse *resp) {
 
     int sockfd;
     struct sockaddr_in serv_addr;
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(host); 
+    serv_addr.sin_addr.s_addr = inet_addr(host);
 
     // Split on commas
     char* token = strtok(port, ",");
 
     int scanned_port = 0;
     size_t buf_len = 0;
-    resp->output = malloc(BUF_LEN);
+    size_t buf_cap = BUF_LEN;
+    resp->output = malloc(buf_cap);
+    resp->output[0] = '\0';
 
-    // Iterate through each port
+    // Iterate through each port (or port range)
     while (token != NULL) {
 
-        // TODO: Range parsing
-        if (strchr(token, '-')) {
-            printf("[-] Will do ranges later (%s)...\n", token);
-            token = strtok(NULL, ",");
-            continue;
-        }
+        char *dash = strchr(token, '-');
 
-        printf("[*] Scanning %s...\n", token);
-        scanned_port = scan(atoi(token), sockfd, serv_addr);
+        if (dash != NULL) {
+            int range_start = atoi(token);
+            int range_end = atoi(dash + 1);
 
-        if (scanned_port != -1) {
-            buf_len += snprintf(resp->output + buf_len, BUF_LEN - buf_len, "%d\n", scanned_port);
+            if (range_start > range_end) {
+                int tmp = range_start;
+                range_start = range_end;
+                range_end = tmp;
+            }
+            if (range_start < 1) range_start = 1;
+            if (range_end > PORT_SCAN_MAX_PORTS) range_end = PORT_SCAN_MAX_PORTS;
+
+            for (int p = range_start; p <= range_end; p++) {
+                printf("[*] Scanning %d...\n", p);
+                scanned_port = scan(p, sockfd, serv_addr);
+
+                if (scanned_port != -1) {
+                    appendPort(&resp->output, &buf_cap, &buf_len, scanned_port);
+                }
+            }
+        } else {
+            printf("[*] Scanning %s...\n", token);
+            scanned_port = scan(atoi(token), sockfd, serv_addr);
+
+            if (scanned_port != -1) {
+                appendPort(&resp->output, &buf_cap, &buf_len, scanned_port);
+            }
         }
 
         token = strtok(NULL, ",");
